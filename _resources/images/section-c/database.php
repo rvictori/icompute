839e07253c77abd84d787fae329821dbb582ca17
Rogelio Victorio Jr <rvictori@cs-520-4-6.local>
2019-04-18 10:11:34 -0400

    Update

---
+++ _resources/images/section-c/database.php
@@ -0,0 +1,8 @@
+<?php
+$host = '35.237.181.128';
+$username = 'root';
+$password = 'iComputeProject';
+$db_name = 'iCompute';
+
+$mysqli = new mysqli($host, $username, $password, $db_name) or die($mysqli->error);
+?>
